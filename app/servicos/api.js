e16ab4faa55e7173d6ab9ff24cb5c04c8a534997
Vitor Santos <vitorvitorsantos2015@hotmail.com>
2026-04-16 22:49:30 -0300

    Remoção dos mocks para listas vindas da API (json-server)

--- app/servicos/api.js
+++ app/servicos/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://192.168.15.7:3000";
+const API_URL = "http://192.168.0.237:3000";
 
 const api = axios.create({
   baseURL: API_URL,
@@ -8,5 +8,3 @@ const api = axios.create({
 });
 
 export default api;
-
-//
