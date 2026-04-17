5eb1be7a1ae7c4738169ca88a5181997a0083896
Vitor Santos <vitorvitorsantos2015@hotmail.com>
2026-04-16 22:52:14 -0300

    tela de perfil profissional

--- App.jsx
+++ App.jsx
@@ -3,11 +3,12 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import MenuLateral from "./app/componentes/MenuLateral/index.jsx";
-import HomePaciente from "./app/telas/HomePaciente/index.jsx";
-import Login from "./app/telas/Login/index.jsx";
-import ResultadosBusca from "./app/telas/ResultadosBusca/index.jsx";
-import SelecaoPerfil from "./app/telas/SelecaoPerfil/index.jsx";
+import MenuLateral from "./app/componentes/MenuLateral";
+import HomePaciente from "./app/telas/HomePaciente";
+import Login from "./app/telas/Login";
+import PerfilProfissional from "./app/telas/PerfilProfissional";
+import ResultadosBusca from "./app/telas/ResultadosBusca";
+import SelecaoPerfil from "./app/telas/SelecaoPerfil";
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -19,28 +20,30 @@ function TabNavigator() {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
-          if (route.name === "HomePaciente") {
+
+          if (route.name === "HomePaciente")
             iconName = focused ? "home" : "home-outline";
-          } else if (route.name === "Mensagens") {
-            iconName = focused ? "chatbubble" : "chatbubble-outline";
-          } else if (route.name === "Consultas") {
+          else if (route.name === "Mensagens")
+            iconName = focused ? "chatbox" : "chatbox-outline";
+          else if (route.name === "Consultas")
             iconName = focused ? "calendar" : "calendar-outline";
-          } else if (route.name === "Favoritos") {
+          else if (route.name === "Favoritos")
             iconName = focused ? "heart" : "heart-outline";
-          }
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
         tabBarActiveTintColor: "#0063c7",
-        tabInactiveTintColor: "#666",
+        tabBarInactiveTintColor: "#666",
         headerShown: false,
       })}
     >
+      {/** TODO: Incluir as telas 'children' de HomePaciente */}
       <Tab.Screen
         name="HomePaciente"
         component={HomePaciente}
         options={{ tabBarLabel: "Home" }}
       />
+      {/* TEMPORÁRIO */}
       <Tab.Screen name="Mensagens" component={ResultadosBusca} />
       <Tab.Screen name="Consultas" component={HomePaciente} />
       <Tab.Screen name="Favoritos" component={HomePaciente} />
@@ -52,9 +55,13 @@ function DrawerNavigator() {
   return (
     <Drawer.Navigator
       drawerContent={(props) => <MenuLateral {...props} />}
-      screenOptions={{ headerShown: false, drawerStyle: { width: "80%" } }}
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: { width: "80%" },
+        drawerType: "front",
+      }}
     >
-      <Drawer.Screen name="TabNavigator" component={TabNavigator} />
+      <Drawer.Screen name="Tabs" component={TabNavigator} />
     </Drawer.Navigator>
   );
 }
@@ -69,6 +76,11 @@ export default function App() {
         <Stack.Screen name="SelecaoPerfil" component={SelecaoPerfil} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Drawer" component={DrawerNavigator} />
+        <Stack.Screen name="ResultadosBusca" component={ResultadosBusca} />
+        <Stack.Screen
+          name="PerfilProfissional"
+          component={PerfilProfissional}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
